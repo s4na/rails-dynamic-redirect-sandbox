6b891fe84b5285e70d6e722f70539db1ce448bd9
s4na <appletea.umauma@gmail.com>
2020-09-14 19:50:10 +0900

    Add sample routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'foo/:hoge_type/:hoge_id', to: redirect('/foo/%{hoge_type}/%{hoge_id}')
 end
